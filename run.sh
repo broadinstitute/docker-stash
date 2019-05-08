1fcc1a1cef0881e04e6a3f2e8d17045e18900538
Andrew Teixeira <teixeira@broadinstitute.org>
2019-05-08 12:48:07 -0400

    Try to fix the proxy

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "${PROXY_NAME}" ]; then
+    export PROXY_NAME='stash'
+fi
+
+if [ -z "${PROXY_PORT}" ]; then
+    export PROXY_PORT='80'
+fi
+
+# Configs to mess with
+/usr/local/bin/configs.sh
+
+RETCODE=$?
+if [ $RETCODE -ne 0 ]; then
+    echo "Unable to setup config files"
+    exit $RETCODE
+fi
+
+exec /opt/atlassian/stash/start-stash.sh -fg 2>&1
